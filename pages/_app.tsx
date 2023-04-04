e642dc495d5962136333a3c9e84e128bb1cc3e21
sj <sjc0829@naver.com>
2023-04-04 19:45:07 +0900

    [MOD] 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,13 +13,13 @@ function MyApp({ Component, pageProps }: AppProps, { userAgent } : userAgentProp
   return (
     <>
       <RecoilRoot>
-        <Head>
+        {/* <Head>
           <title>비블록 투자자보호센터</title>
           <link rel='canonical' href='https://trust.beeblock.co.kr'></link>
           <meta name='description' content='BeeTrust / 가상자산 주간 리포트 / 비블록 신문고 등 가상자산 시장의 질서를 수호하는 안전 거래의 기점'/>
           <link rel="icon" href="/favicon.ico" />
           <meta name="google-site-verification" content="cwAFUKPT90HvtGqLqGqGCR9SCw1PvOj52-qEji0-13M" />
-        </Head>
+        </Head> */}
         <Layout>
           <Component {...pageProps} />
         </Layout>
